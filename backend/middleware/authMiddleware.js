edfeaf72548d1cb3d539906aae7474b91a8dc1ac
LE TAN LOC <letanlocjp1001@gmail.com>
2022-02-08 23:36:50 +0900

    redux tookit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,7 +11,8 @@ const protect = asyncHandler(async (req, res, next) => {
   ) {
     try {
       // Get token from header
-      token = req.headers.authorization.split('')[1]
+      token = req.headers.authorization.split(' ')[1]
+      console.log(token)
       // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
       // Get user from token
